38f3acd560496fd2456962696cd04cd86a212c16
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2022-07-31 13:59:38 +0300

    Create Car.java

---
+++ src/main/java/Car.java
@@ -0,0 +1,36 @@
+public class Car {
+  
+  private String licenseNum;
+  private boolean hadAcciden‬‬t;
+  private int price;
+  
+  public Car(‫‪String licenseNum, boolean hadAccident, int price) {
+    this.licenseNum = licenseNum;
+    this.hadAccident = hadAccident;
+    this.price = price;
+  }
+  
+  public String getLicenseNum() {
+    return licenseNum;
+  }
+  
+  public void setLicenseNum(int licenseNum) {
+    this.licenseNum = licenseNum;
+  }
+  
+  public boolean hadAccident() {
+    reutrn hadAccident;
+  }
+  
+  public void setHadAccident(boolean hadAccident) {
+    this.hadAccident = hadAccident;
+  }
+  
+  public int getPrice() {
+    return price;
+  }
+  
+  public void setPrice(int price) {
+    this.price = price;
+  }
+}
