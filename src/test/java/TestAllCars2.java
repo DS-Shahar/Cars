c192ca4a2a99159b52d520b8abe4b75a8278d4f4
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2022-07-31 14:12:17 +0300

    Create TestAllCars2.java

---
+++ src/test/java/TestAllCars2.java
@@ -0,0 +1,33 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class TestAllCars2 {
+
+   @Test
+   public void testB2()
+   {
+      AllCars a = new AllCars(2);
+      
+      Car c;
+      boolean b;
+      
+      c = new Car("8080488", false, 95_000);
+      b = a.addCar(c);
+      assertEquals(b, true);
+
+      c = new Car("9911485", true, 50_000);
+      b = a.addCar(c);
+      assertEquals(b, true);
+      
+      c = new Car("7588321", false, 75_000);
+      b = a.addCar(c);
+      assertEquals(b, false);
+      
+      a.print(70_000, 80_000);
+   }
+}
